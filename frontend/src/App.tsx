43b1714959f3c9adf8b8747514760559b017d4f0
jade diniz <jadedinizpro@gmail.com>
2026-05-06 11:59:03 -0300

    feat: Ajustes de rota

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,14 +2,15 @@ import { Login } from "./pages/Login";
 import AtividadeFormPage from "./pages/AtividadeFormPage"
 import { ActivitiesActive } from "./pages/ActivitiesActive";
 import { ActivitiesDone } from "./pages/ActivitiesDone";
+import { Home } from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/atividades" element={<ActivitiesActive />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/atividades" element={<Home />} />
+        <Route path="/atividades/ativas" element={<ActivitiesActive />} />
         <Route path="/atividades/concluidas" element={<ActivitiesDone />} />
         <Route path="/atividades/:id" element={<AtividadeFormPage />} />
         <Route path="/atividades/new" element={<AtividadeFormPage />} />
